e4af88bedd72ce3bd926e071a706440bacbc004f
Ilenia M <nethleen@gmail.com>
2022-03-09 09:40:27 +0100

    module weapons

---
+++ lists/weapons.js
@@ -0,0 +1,6 @@
+const weapons = "club, dagger, greatclub, handaxe, javelin, light hammer, mace, quarterstaff, sickle, spear, light crossbow, dart, shortbow, sling, battleaxe, flail, glaive, greatsword, halberd, lance, longsword, maul, mornignstar, pike, rapier, scimitar, shortsword, trident, war pick, warhammer, whip, blowgun, hand crossbow, heavy crossbow, longbow".split(
+    ", "
+  );
+const swords = ["greatsword", "longsword", "shortsword", "scimitar", "rapier"];
+
+export {weapons, swords};
