83a5e846e8cefbdab5f9d30048a35d1d1c329c64
Ilenia M <nethleen@gmail.com>
2022-03-09 09:38:57 +0100

    module numberCoins

---
+++ utils/numberOfCoins.js
@@ -0,0 +1,11 @@
+import {rwn} from "./randomWholeNumber.js"
+
+function numberCoins(n, d, m) {
+  let c = 1;
+  for (let i = 0; i < n; i++) {
+    c += rwn(d);
+  }
+  return c * m;
+}
+
+export { numberCoins }
