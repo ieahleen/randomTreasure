0576d514bba1155da64fe08a0403b86a728bf8b1
Ilenia M <nethleen@gmail.com>
2022-03-09 14:53:13 +0100

    fix syntax error

--- script.js
+++ script.js
@@ -14,24 +14,6 @@ import { treasureA } from "./individualTreasureTables/challenge4.js";
 
 const targetElement = document.querySelector("#t");
 
-function treasureA() {
-	const t = rwn(100);
-	const treasure = {};
-	if (t <= 30) {
-		treasure.CP = numberCoins(5, 6, 1);
-	} else if (t <= 60) {
-		treasure.SP = numberCoins(4, 6, 1);
-	} else if (t <= 70) {
-		treasure.EP = numberCoins(3, 6, 1);
-	} else if (t <= 95) {
-		treasure.GP = numberCoins(3, 6, 1);
-	} else {
-		treasure.PP = numberCoins(1, 6, 1);
-	}
-  
-	return treasure;
-}
-
 function treasureB() {
 	const t = rwn(100);
 	const treasure = {};
