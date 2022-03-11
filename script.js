d47ce8a174f98c0a190cdbd406d40cfb06412056
Ilenia M <nethleen@gmail.com>
2022-03-11 11:15:52 +0100

    fixed missing imports

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ import { treasureA } from "./individualTreasureTables/challenge0-4.js";
 import { treasureB } from "./individualTreasureTables/challenge5-10.js";
 import { treasureC } from "./individualTreasureTables/challenge11-16.js";
 import { treasureD } from "./individualTreasureTables/challenge17+.js";
+import { treasureE } from "./trasureHoardTables/challenge0-4.js";
+import { treasureF } from "./trasureHoardTables/challenge5-10.js";
+import { treasureG } from "./trasureHoardTables/challenge11-16.js";
+import { treasureH } from "./trasureHoardTables/challenge17+.js";
 
 const targetElement = document.querySelector("#t");
 
@@ -11,3 +15,5 @@ document.querySelector("#c").addEventListener("click", () => targetElement.textC
 document.querySelector("#d").addEventListener("click", () => targetElement.textContent = JSON.stringify(treasureD(), null, 2));
 document.querySelector("#e").addEventListener("click", () => targetElement.textContent = JSON.stringify(treasureE(), null, 2));
 document.querySelector("#f").addEventListener("click", () => targetElement.textContent = JSON.stringify(treasureF(), null, 2));
+document.querySelector("#g").addEventListener("click", () => targetElement.textContent = JSON.stringify(treasureG(), null, 2));
+document.querySelector("#h").addEventListener("click", () => targetElement.textContent = JSON.stringify(treasureH(), null, 2));
