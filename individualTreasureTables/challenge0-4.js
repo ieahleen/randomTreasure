61ac22cdbf13f3570999a7c4d3d035a39d42a5b3
ieahleen <nethleen@gmail.com>
2022-06-22 17:41:17 +0000

    refactor: challenge 0-4 individual #5

--- individualTreasureTables/challenge0-4.js
+++ individualTreasureTables/challenge0-4.js
@@ -1,27 +1,24 @@
-import { diceThrow, randomItemFromTableCompound } from '../utils/helpers.js';
-import { randomWholeNumber } from '../utils/helpers.js';
+import { diceThrow, randomItemFromTable } from '../utils/helpers.js';
 
-const randomTreasure = {
-  30: () => ({
-    CP: diceThrow(5, 6),
-  }),
-  60: () => ({
-    SP: diceThrow(4, 6),
-  }),
-  70: () => ({
-    EP: diceThrow(3, 6),
-  }),
-  95: () => ({
-    GP: diceThrow(3, 6),
-  }),
-  100: () => ({
-    PP: diceThrow(1, 6),
-  }),
-};
+const randomTreasure = [
+  100,
+  {
+    30: () => ({
+      CP: diceThrow(5, 6),
+    }),
+    60: () => ({
+      SP: diceThrow(4, 6),
+    }),
+    70: () => ({
+      EP: diceThrow(3, 6),
+    }),
+    95: () => ({
+      GP: diceThrow(3, 6),
+    }),
+    100: () => ({
+      PP: diceThrow(1, 6),
+    }),
+  },
+];
 
-export function treasureA() {
-  return randomItemFromTableCompound(randomTreasure)(
-    randomWholeNumber(100),
-    {}
-  );
-}
+export const treasureA = randomItemFromTable(...randomTreasure);
