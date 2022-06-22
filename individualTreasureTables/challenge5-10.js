4fe7971b2bcd6f7a52ce0c2c1e57a8cafa1ca0dc
ieahleen <nethleen@gmail.com>
2022-06-22 17:48:45 +0000

    refactor: challenge 5-10 individual #5

--- individualTreasureTables/challenge5-10.js
+++ individualTreasureTables/challenge5-10.js
@@ -1,23 +1,14 @@
-import { diceThrow, randomWholeNumber } from '../utils/helpers.js';
+import { diceThrow, randomItemFromTable } from '../utils/helpers.js';
 
-export function treasureB() {
-  const t = randomWholeNumber(100);
-  const treasure = {};
-  if (t <= 30) {
-    treasure.CP = diceThrow(4, 6, 100);
-    treasure.EP = diceThrow(1, 6, 10);
-  } else if (t <= 60) {
-    treasure.SP = diceThrow(6, 6, 10);
-    treasure.GP = diceThrow(2, 6, 10);
-  } else if (t <= 70) {
-    treasure.EP = diceThrow(3, 6, 10);
-    treasure.GP = diceThrow(2, 6, 10);
-  } else if (t <= 95) {
-    treasure.GP = diceThrow(4, 6, 10);
-  } else {
-    treasure.GP = diceThrow(2, 6, 10);
-    treasure.PP = diceThrow(3, 6);
-  }
+const randomTreasure = [
+  100,
+  {
+    30: () => ({ CP: diceThrow(4, 6, 100), EP: diceThrow(1, 6, 10) }),
+    60: () => ({ SP: diceThrow(6, 6, 10), GP: diceThrow(2, 6, 10) }),
+    70: () => ({ EP: diceThrow(3, 6, 10), GP: diceThrow(2, 6, 10) }),
+    95: () => ({ GP: diceThrow(4, 6, 10) }),
+    100: () => ({ GP: diceThrow(2, 6, 10), PP: diceThrow(3, 6) }),
+  },
+];
 
-  return treasure;
-}
+export const treasureB = randomItemFromTable(...randomTreasure);
