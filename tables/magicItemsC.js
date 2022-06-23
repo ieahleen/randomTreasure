4361d05cfdb5b173b20bdc122240cca682db8fbd
ieahleen <nethleen@gmail.com>
2022-06-23 19:42:18 +0000

    refactor: magicItemsC #5

--- tables/magicItemsC.js
+++ tables/magicItemsC.js
@@ -1,75 +1,45 @@
-import { randomWholeNumber } from '../utils/helpers.js';
+import { randomItemFromTable, randomWholeNumber } from '../utils/helpers.js';
 import { spells4, spells5 } from '../lists/spells.js';
 import {
-  randomItemFromArray,
+  randomItemFromArraySimple,
   randomFeatherToken,
   randomCreatureType,
 } from '../utils/helpers.js';
 
-export function magicItemsC() {
-  const n = randomWholeNumber(100);
-  if (n <= 15) return 'Potion of superior healing';
-  if (n <= 22) return `${spells4()} spell scroll (4th level)`;
-  if (n <= 27)
-    return `Ammunition, +2 ${randomItemFromArray(
-      'arrow',
-      'blowgun needle',
-      'crossbow bolt',
-      'sling bullet'
-    )}`;
-  if (n <= 32) return 'Potion of clairvoyance';
-  if (n <= 37) return 'Potion of diminuition';
-  if (n <= 42) return 'Potion of gaseous form';
-  if (n <= 47) return 'Potion of frost giant strength';
-  if (n <= 52) return 'Potion of stone giant strength';
-  if (n <= 57) return 'Potion of heroism';
-  if (n <= 62) return 'Potion of invulnerability';
-  if (n <= 67) return 'Potion of mind reading';
-  if (n <= 72) return `${spells5()} spell scroll (5th level)`;
-  switch (n) {
-    case 73:
-    case 74:
-    case 75:
-      return 'Elixir of health';
-    case 76:
-    case 77:
-    case 78:
-      return 'Oil of etherealness';
-    case 79:
-    case 80:
-    case 81:
-      return 'Potion of fire giant strength';
-    case 82:
-    case 83:
-    case 84:
-      return `Quaal's feather token, ${randomFeatherToken()}`;
-    case 85:
-    case 86:
-    case 87:
-      return `Scroll of protection, ${randomCreatureType()}`;
-    case 88:
-    case 89:
-      return 'Bag of beans';
-    case 90:
-    case 91:
-      return 'Bead of force';
-    case 92:
-      return 'Chime of opening';
-    case 93:
-      return 'Decanter of endless water';
-    case 94:
-      return 'Eyes of minute seeing';
-    case 95:
-      return 'Folding boat';
-    case 96:
-      return "Heward's handy haversack";
-    case 97:
-      return 'Horseshoes of speed';
-    case 98:
-      return 'Necklace of fireballs';
-    case 99:
-      return 'Periapt of health';
-    case 100:
-      return 'Sending stones';
-  }
-}
+const magicItems = {
+  15: 'Potion of superior healing',
+  22: `${spells4()} spell scroll (4th level)`,
+  27: `Ammunition, +2 ${randomItemFromArraySimple(
+    'arrow',
+    'blowgun needle',
+    'crossbow bolt',
+    'sling bullet'
+  )}`,
+  32: 'Potion of clairvoyance',
+  37: 'Potion of diminuition',
+  42: 'Potion of gaseous form',
+  47: 'Potion of frost giant strength',
+  52: 'Potion of stone giant strength',
+  57: 'Potion of heroism',
+  62: 'Potion of invulnerability',
+  67: 'Potion of mind reading',
+  72: () => `${spells5()} spell scroll (5th level)`,
+  75: 'Elixir of health',
+  78: 'Oil of etherealness',
+  81: 'Potion of fire giant strength',
+  84: () => `Quaal's feather token, ${randomFeatherToken()}`,
+  87: () => `Scroll of protection, ${randomCreatureType()}`,
+  89: 'Bag of beans',
+  91: 'Bead of force',
+  92: 'Chime of opening',
+  93: 'Decanter of endless water',
+  94: 'Eyes of minute seeing',
+  95: 'Folding boat',
+  96: "Heward's handy haversack",
+  97: 'Horseshoes of speed',
+  98: 'Necklace of fireballs',
+  99: 'Periapt of health',
+  100: 'Sending stones',
+};
+
+export const magicItemsC = randomItemFromTable(100, magicItems);
